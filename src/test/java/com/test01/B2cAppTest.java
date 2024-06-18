7fc55a768ca542bab9f7059486e8a948b9774e34
Adarsh9810 <r.adarsh23593@gmail.com>
2024-06-18 22:07:17 +0530

    This is first Commit

---
+++ src/test/java/com/test01/B2cAppTest.java
@@ -0,0 +1,23 @@
+package com.test01;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class B2cAppTest {
+
+	@Test
+	public void testlogin1()
+	{
+		b2cApp myapp=new b2cApp();
+		Assert.assertEquals(0,myapp.userLogin("abc","abc1234"));
+		
+	}
+	
+	@Test
+	public void testlogin2()
+	{
+		b2cApp myapp=new b2cApp();
+		Assert.assertEquals(1,myapp.userLogin("abc","abc@1234"));
+	}
+}
